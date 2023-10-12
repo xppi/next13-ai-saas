1845d63b73f1b7eeabb72280ff097ffd57efb561
xppi <59060461+xppi@users.noreply.github.com>
2023-10-12 16:49:03 +0200

    dawd

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const testimonials = [
   {
-    name: "Joel",
+    name: "Philipp",
     avatar: "J",
     title: "Software Engineer",
     description: "This is the best application I've ever used!",
